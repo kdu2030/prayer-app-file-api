bef0072cc562756baef3d9bcbbe3ca5cd4031c86
kdu2030 <kdu2030@gmail.com>
2024-03-02 15:07:50 -0500

    Remove Test Routes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask_restx import Api, Resource
-from test import test_api
+from flask_restx import Api
 from file_manager import file_upload_namespace
 from socket import gethostname
 from constants import LOCAL_API_URL
@@ -10,7 +9,6 @@ api = Api(app, version="1.0", title="Prayer App File Upload API")
 is_prod = 'liveconsole' in gethostname()
 app.config["APP_URL"] = LOCAL_API_URL
 
-api.add_namespace(test_api)
 api.add_namespace(file_upload_namespace)
 
 
